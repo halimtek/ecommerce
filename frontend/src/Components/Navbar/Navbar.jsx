9e238204f0571dac37ce53e2d316399e39beacdc
HackingtheHack <abdihalim487@gmail.com>
2026-06-17 10:22:53 +0300

    logo updated

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useRef, useState } from 'react'
 import './/Navbar.css'
-import logo from '../Assets/logo.png'
+import logo from '../Assets/logo.jpg'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
@@ -22,7 +22,7 @@ export const Navbar = () => {
     <div className='navbar'>
        <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>HalimOnlineShopping</p>
         </div>
         <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
         <ul ref={menuRef} className="nav-menu">
